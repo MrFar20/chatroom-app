1bfccafc2d7668fb0e2172c132e14d205bb68284
wanghaoxin <a706519033@qq.com>
2019-08-20 10:25:31 +0800

    before merge

--- src/main/java/pers/mrwangx/tools/chatroom/App.java
+++ src/main/java/pers/mrwangx/tools/chatroom/App.java
@@ -1,8 +1,5 @@
 package pers.mrwangx.tools.chatroom;
 
-import static org.fusesource.jansi.Ansi.*;
-import static org.fusesource.jansi.Ansi.Color.*;
-
 /**
  * @description:
  * @author: 王昊鑫
@@ -11,12 +8,11 @@ import static org.fusesource.jansi.Ansi.Color.*;
 public class App {
 
 	public static void main(String[] args) {
-		// System.out.println(ansi().eraseScreen().fg(RED).a("HELLO").fg(YELLOW).a(",WORLD").reset());
 		if (args == null || args.length < 3) {
 			System.out.println("请输入正确格式: server/client host port");
 		} else if (args[0].equals("server")) {
 			AppServer.run(args[1], Integer.parseInt(args[2]));
-		} else if (args[1].equals("client")) {
+		} else if (args[0].equals("client")) {
 			AppClient.run(args[1], Integer.parseInt(args[2]));
 		}
 
